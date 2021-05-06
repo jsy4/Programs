46233d343f676c171a2b82c860f5c85c4babebbd
jsy4 <65105285+jsy4@users.noreply.github.com>
2021-05-05 23:58:06 -0600

    Update Circle2.java

--- Circles/src/edu/nmsu/cs/circles/Circle2.java
+++ Circles/src/edu/nmsu/cs/circles/Circle2.java
@@ -13,7 +13,9 @@ public class Circle2 extends Circle
 		double d;
 		d = Math.sqrt(Math.pow(center.x - other.center.x, 2) +
 				Math.pow(center.y - other.center.y, 2));
-		if (d <= radius + other.radius)
+		if(d > Math.abs(radius-other.radius))
+			return false;
+		else if (d <= radius + other.radius)
 			return true;
 		else
 			return false;
